8665e5a0f3e161e8ff5d13db78203c1f1657b4da
antoniovalentic <antoniomax98@gmail.com>
2024-08-18 19:17:48 +0200

    Renamed RenderViewport

--- scripts/camera_viewport.gd
+++ scripts/camera_viewport.gd
@@ -1,5 +1,5 @@
 @tool
-class_name UIViewport
+class_name RenderViewport
 extends SubViewportContainer
 
 ## The camera this viewport replicates.
@@ -16,7 +16,4 @@ func _ready():
 
 func _process(_delta: float) -> void:
     if view_camera and camera:
-        # Update any properties to copy the camera exactly.
-        # I don't plan on changing anything put the transform at runtime,
-        # but if you have more camera properties that would change at runtime, add them below.
         camera.global_transform = view_camera.global_transform
