cd25f656685be54944de061e7d9d6d87e4fcb35e
antoniovalentic <antoniomax98@gmail.com>
2024-09-03 18:59:44 +0200

    Main menu audio

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var pause_menu: PauseMenu = $PauseMenu
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready():
     Global.set_current_scene(self)
@@ -9,6 +10,7 @@ func _ready():
     Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
     pause_menu.resume_pressed.connect(_on_resume_button_pressed)
     pause_menu.quit_pressed.connect(_on_quit_button_pressed)
+    audio_player.play()
 
 func _on_resume_button_pressed():
     Global.set_pause_game(false)
